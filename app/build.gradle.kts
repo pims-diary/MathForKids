2c9088b5e6eb9e82046e0587cf403acaaedf41e0
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2024-11-14 00:20:17 +1300

    Add dependency for preferences datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
