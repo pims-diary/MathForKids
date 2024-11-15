bd90e36a8effa7cf4f07eaa62f8a3d27ee9cda34
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2024-11-15 23:38:13 +1300

    Add coroutine dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
