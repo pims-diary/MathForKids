199be1bb5d9b97dd1eda04bf67524b9f660a7ab0
abir4u <abirpal90@gmail.com>
2024-11-21 01:57:27 +1300

    Create dataStore functions for player Id

--- app/src/main/java/com/example/math_for_kids/storage/QuizStorage.kt
+++ app/src/main/java/com/example/math_for_kids/storage/QuizStorage.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -13,11 +14,19 @@ import java.io.IOException
 // Define DataStore and Preference keys
 val Context.dataStore by preferencesDataStore("user_prefs")
 val QUIZ_LEVEL = intPreferencesKey("level")
+val PLAYER_ID = stringPreferencesKey("id")
 
-// Function to save data
+// Function to save level
 suspend fun updateLevel(context: Context, level: Int) {
     context.dataStore.edit { preferences ->
         preferences[QUIZ_LEVEL] = level
+    }
+}
+
+// Function to save player id
+suspend fun savePlayerId(context: Context, id: String) {
+    context.dataStore.edit { preferences ->
+        preferences[PLAYER_ID] = id
     }
 }
 
@@ -29,5 +38,16 @@ fun getLevel(context: Context): Flow<Int> {
         }
         .map { preferences ->
             preferences[QUIZ_LEVEL] ?: 1
+        }
+}
+
+// Function to get data
+fun getPlayerId(context: Context): Flow<String> {
+    return context.dataStore.data
+        .catch { exception ->
+            if (exception is IOException) emit(emptyPreferences())
+        }
+        .map { preferences ->
+            preferences[PLAYER_ID] ?: ""
         }
 }
