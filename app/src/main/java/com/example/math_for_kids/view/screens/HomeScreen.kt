ef6bbf69d747b22d482ead6c139e401459a47176
abir4u <abirpal90@gmail.com>
2024-11-22 23:49:43 +1300

    Use stored player ID to make GET player call, and store player level to use in Quiz

--- app/src/main/java/com/example/math_for_kids/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/math_for_kids/view/screens/HomeScreen.kt
@@ -4,14 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -19,8 +22,11 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.math_for_kids.navigations.AuthPages
+import com.example.math_for_kids.storage.getPlayerId
+import com.example.math_for_kids.storage.savePlayerId
 import com.example.math_for_kids.storage.updateLevel
 import com.example.math_for_kids.viewmodel.PlayerDetailsViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @Composable
@@ -32,17 +38,33 @@ fun HomeScreen(
     val scope = rememberCoroutineScope()
     // Observe the state of responseBody directly
     val response = playerDetailsViewModel.responseBody
+    var playerId by remember { mutableStateOf("") }
 
-    // Trigger getPlayer only once using LaunchedEffect
+    // Get Player ID from sharedPreference dataStore
     LaunchedEffect(Unit) {
-        playerDetailsViewModel.getPlayer("10001")
+        scope.launch {
+            playerId = getPlayerId(context).first()
+        }
     }
 
-    // Trigger the datastore update when response updates
+    // Wait until Player ID is received from dataStore
+    LaunchedEffect(playerId) {
+        if (playerId.isNotEmpty()) { // Ensure playerId is not empty before calling getPlayer
+            // Make get-player API call
+            scope.launch {
+                playerDetailsViewModel.getPlayer(playerId)
+            }
+        }
+    }
+
+    // Wait for API response
     LaunchedEffect(response) {
         if (response.isNotEmpty()) {
+            // Extract Player Level from API response
             val levelString = response["level"] as? String ?: "0"
+            // Convert Player Level to Int
             val level = levelString.toIntOrNull() ?: 0
+            // Save Player Level in sharedPreference dataStore
             scope.launch {
                 updateLevel(context, level)
             }
@@ -64,12 +86,14 @@ fun HomeScreen(
             Text("Welcome, $name!")
             Text("Email: $email")
             Text("Level: $levelString")
+            Text("Player ID: $playerId")
 
             Button(onClick = {
                 navHostController.navigate(AuthPages.Login.route)
                 // Clear dataStore on Logout
                 scope.launch {
                     updateLevel(context, 0)
+                    savePlayerId(context, "")
                 }
             }) {
                 Text("Logout")
@@ -79,6 +103,7 @@ fun HomeScreen(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
+                // Show Loading Indicator until API response is received
                 CircularProgressIndicator()
             }
         }
--- app/src/main/java/com/example/math_for_kids/view/screens/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/math_for_kids/view/screens/authentication/LoginScreen.kt
@@ -4,22 +4,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import com.example.math_for_kids.navigations.AuthPages
+import com.example.math_for_kids.storage.savePlayerId
 import com.example.math_for_kids.view.components.AuthenticationForm
 import com.example.math_for_kids.view.components.LinkButton
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(navHostController: NavHostController) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         AuthenticationForm("Login") {
-            navHostController.navigate(AuthPages.AppLanding.route)
+            scope.launch {
+                savePlayerId(context, "10001")
+                navHostController.navigate(AuthPages.AppLanding.route)
+            }
         }
         LinkButton(
             text = "Register Now",
