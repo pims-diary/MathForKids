d1aaf3ecbbd171f43477ede4577940db81289107
abir4u <abirpal90@gmail.com>
2024-11-24 13:33:47 +1300

    Create AuthenticationViewModel with login function

---
+++ app/src/main/java/com/example/math_for_kids/viewmodel/AuthenticationViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.math_for_kids.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.math_for_kids.database.api.nonsql.UpdateLevel
+import com.example.math_for_kids.database.api.sql.LoginCreds
+import com.example.math_for_kids.database.instance.RetrofitInstance
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+class AuthenticationViewModel : ViewModel() {
+    var responseBody by mutableStateOf<Map<String, Any>>(emptyMap())
+    private var errorMessage by mutableStateOf("")
+    private var code by mutableIntStateOf(0)
+    private var isSuccess by mutableStateOf(false)
+
+    fun login(email: String, password: String) {
+        val requestBody = LoginCreds(email, password)
+        viewModelScope.launch {
+            try {
+                val response = RetrofitInstance.sqlApiServices.login(requestBody)
+                isSuccess = response.isSuccessful
+                responseBody = response.body() ?: emptyMap()
+                code = response.code()
+            } catch (e: IOException) {
+                isSuccess = false
+                errorMessage = "Network Error: ${e.message}"
+            } catch (e: HttpException) {
+                isSuccess = false
+                errorMessage = "HTTP Error: ${e.message}"
+            } catch (e: Exception) {
+                isSuccess = false
+                errorMessage = "Unknown Server Error: ${e.message}"
+            }
+        }
+    }
+
+}
