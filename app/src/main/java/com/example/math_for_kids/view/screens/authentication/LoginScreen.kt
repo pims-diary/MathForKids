72d55e7d56d4a289c4ebf08ba79862a465b556ef
abir4u <abirpal90@gmail.com>
2024-11-24 18:11:08 +1300

    Autologin a User that has not logged out from the app

--- app/src/main/java/com/example/math_for_kids/view/screens/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/math_for_kids/view/screens/authentication/LoginScreen.kt
@@ -4,23 +4,41 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.math_for_kids.navigations.AuthPages
+import com.example.math_for_kids.storage.getPlayerId
 import com.example.math_for_kids.storage.savePlayerId
 import com.example.math_for_kids.view.components.AuthenticationForm
 import com.example.math_for_kids.view.components.LinkButton
 import com.example.math_for_kids.viewmodel.AuthenticationViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(navHostController: NavHostController, authViewModel: AuthenticationViewModel = viewModel()) {
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
+    var playerId by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        playerId = getPlayerId(context).first()
+    }
+
+    LaunchedEffect(playerId) {
+        if (playerId.isNotBlank()) {
+            navHostController.navigate(AuthPages.AppLanding.route)
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -30,7 +48,7 @@ fun LoginScreen(navHostController: NavHostController, authViewModel: Authenticat
         AuthenticationForm("Login", authViewModel) { responseBody, isSuccess ->
             if (isSuccess) {
                 scope.launch {
-                    val playerId = responseBody["PlayerId"].toString()
+                    playerId = responseBody["PlayerId"].toString()
                     savePlayerId(context, responseBody["PlayerId"].toString())
                     navHostController.navigate(AuthPages.AppLanding.route)
                 }
