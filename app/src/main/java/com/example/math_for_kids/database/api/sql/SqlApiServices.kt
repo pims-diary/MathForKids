c419f42465b04ababaa57bb958702bb332632da7
abir4u <abirpal90@gmail.com>
2024-11-24 13:32:33 +1300

    Create the SQL API Login service

--- app/src/main/java/com/example/math_for_kids/database/api/sql/SqlApiServices.kt
+++ app/src/main/java/com/example/math_for_kids/database/api/sql/SqlApiServices.kt
@@ -1,4 +1,14 @@
 package com.example.math_for_kids.database.api.sql
 
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+data class LoginCreds(var email: String, var password: String)
+
 interface SqlApiServices {
+    @POST
+    suspend fun login(
+        @Body body: LoginCreds
+    ): Response<Map<String, Any>>
 }
