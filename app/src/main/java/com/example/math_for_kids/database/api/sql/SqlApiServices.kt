301629c14349ca9ca9851d7e77894c9f6b9598aa
abir4u <abirpal90@gmail.com>
2024-11-24 15:24:24 +1300

    Fix the POST API service with correct URL and body serialisation

--- app/src/main/java/com/example/math_for_kids/database/api/sql/SqlApiServices.kt
+++ app/src/main/java/com/example/math_for_kids/database/api/sql/SqlApiServices.kt
@@ -1,13 +1,17 @@
 package com.example.math_for_kids.database.api.sql
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-data class LoginCreds(var email: String, var password: String)
+data class LoginCreds(
+    @SerializedName("Email") var email: String,
+    @SerializedName("Password") var password: String
+)
 
 interface SqlApiServices {
-    @POST
+    @POST("sqlite/login")
     suspend fun login(
         @Body body: LoginCreds
     ): Response<Map<String, Any>>
